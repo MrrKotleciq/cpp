60324b56c9cb51e8435be5880777b64a057942be
Kornel Łaba <kornellaba44@gmail.com>
2025-11-21 18:24:08 +0100

    Update main1.c

--- c/main1.c
+++ c/main1.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main()
+/*int main()
 {
     printf("Hello World");
+    return 0;
+}*/
+int main() {
+    int num = 7;
+    char ch = 'Z';
+    float pi = 3.14;
+
     return 0;
 }
